9d95a6bf34c89ea4c4c82035c99d86aca4bf601c
JosephASandoval <josephasandoval17@gmail.com>
2021-02-23 20:13:48 -0800

    updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
-const mongoose = require('mongoose');
+const db = require("./config/keys").mongoURI;
+const mongoose = require("mongoose");
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const bodyParser = require("body-parser");
@@ -9,19 +9,28 @@ const User = require("./models/User");
 const passport = require("passport");
 const path = require("path");
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 mongoose
-    .connect(db, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-    })
-    .then(() => console.log('Connected to MongoDB successfully'))
-    .catch(err => console.log(err));
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req,res) => {res.send('Hello World')});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.use(passport.initialize());
 require("./config/passport")(passport);
@@ -30,5 +39,6 @@ app.use("/api/users", users);
 app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {console.log(`Server is running on port ${port}`)});
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
