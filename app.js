cc8894647dd791f347ffe32e87ec692be09a2ea1
timjkim210 <timjkim210@gmail.com>
2021-02-25 14:23:50 -0800

    hello

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const bodyParser = require("body-parser");
@@ -20,7 +20,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
