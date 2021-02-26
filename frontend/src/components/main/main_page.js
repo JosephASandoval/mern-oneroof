9224c8423156da81edbf0ca4d04d4946fac9e96b
anyqx <annieyxu@berkeley.edu>
2021-02-26 13:46:25 -0800

    mainpage css

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import '../../styles/main_page.css'
 
 class MainPage extends React.Component {
 
   render() {
     return (
+      <>
+      <div className='main-page-container'>
+        <h1> Welcome to OneRoof</h1>
+        <ul className='welcome-msg'>
+          <li>OneRoof, a housemate organization tool, is an online application 
+            for users create a home and invite housemates. With this app, 
+            housemates can better coordinate house chores, expenses, and communication.
+          </li>
+          <li><img src='logo_word.png' alt='LOGO' className='logo-img-word'/></li>
+        </ul>
+      </div>
       <div>
-        <h1>OneRoof</h1>
         <footer>
-          Copyright &copy; 2021 OneRoof
+          <p>Copyright &copy; 2021 ONEROOF</p>
         </footer>
       </div>
+      </>
     );
   }
 }
