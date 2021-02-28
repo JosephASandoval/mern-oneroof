50b2ffbba7b7969ac8ea9f43c02382755f844ca9
JosephASandoval <josephasandoval17@gmail.com>
2021-02-28 15:57:39 -0800

    added a guest user option

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import '../../styles/login_form.css'
+import "../../styles/login_form.css";
 
 class LoginForm extends React.Component {
   constructor(props) {
@@ -14,6 +14,7 @@ class LoginForm extends React.Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.renderErrors = this.renderErrors.bind(this);
+    this.handleGuest = this.handleGuest.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -42,11 +43,22 @@ class LoginForm extends React.Component {
     this.props.login(user);
   }
 
+  handleGuest(e) {
+    e.preventDefault();
+    let user = {
+      email: "guest@gmail.com",
+      password: "guest",
+    };
+    this.props.login(user);
+  }
+
   renderErrors() {
     return (
-      <ul className='session-errors-container'>
+      <ul className="session-errors-container">
         {Object.keys(this.state.errors).map((error, i) => (
-          <li className='session-errors' key={`error-${i}`}>{this.state.errors[error]}</li>
+          <li className="session-errors" key={`error-${i}`}>
+            {this.state.errors[error]}
+          </li>
         ))}
       </ul>
     );
@@ -57,25 +69,36 @@ class LoginForm extends React.Component {
       <div className="login-form-container">
         <h2> LOG IN </h2>
         <div>
-        <form className='entry-form' onSubmit={this.handleSubmit}>
+          <form className="entry-form" onSubmit={this.handleSubmit}>
             <br />
-            <input className='form-input'
+            <input
+              className="form-input"
               type="text"
               value={this.state.email}
               onChange={this.update("email")}
               placeholder="Email"
             />
             <br />
-            <input className='form-input'
+            <input
+              className="form-input"
               type="password"
               value={this.state.password}
               onChange={this.update("password")}
               placeholder="Password"
             />
             <br />
-            <input className='submit-button' type="submit" value="Submit" />
-            {this.renderErrors()}
-        </form>
+            <input className="submit-button" type="submit" value="Submit" />
+            <button className="submit-button input-field">Login</button>
+            <br />
+            <button
+              className="submit-button input-field"
+              onClick={this.handleGuest}
+            >
+              Guest Login
+            </button>
+
+            <div className="">{this.renderErrors()}</div>
+          </form>
         </div>
       </div>
     );
