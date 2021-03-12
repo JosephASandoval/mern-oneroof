25253f8dfc27ed57aad3d92083932a35df3e6561
timjkim210 <timjkim210@gmail.com>
2021-03-12 11:18:39 -0800

    working copy of main

--- frontend/src/components/expenses/expense_list_item.js
+++ frontend/src/components/expenses/expense_list_item.js
@@ -5,6 +5,9 @@ const ExpenseListItem = props => {
         <li>
             <h4>{props.expense.body + ' ' + props.expense.amount}</h4>
             <button className="delete" onClick={() => props.destroyExpense(props.expense._id)}>Delete this Expense</button>
+            <label> Paid
+            <input type="checkbox"></input>
+            </label>
         </li>
     )
 }
