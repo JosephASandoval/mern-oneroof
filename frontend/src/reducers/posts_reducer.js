8cc41bbedef666e67f76dcad5f9770d8afa23eca
timjkim210 <timjkim210@gmail.com>
2021-03-16 10:55:18 -0700

    changed reducer to allow deletion of other users post

--- frontend/src/reducers/posts_reducer.js
+++ frontend/src/reducers/posts_reducer.js
@@ -31,11 +31,14 @@ const PostsReducer = (
       })[0];
       let idx = newState.all.indexOf(postToDelete);
       if (idx !== -1) newState.all.splice(idx, 1);
-      let userPostToDelete = newState.user.filter(post => {
+      if (newState.user.includes(postToDelete)) {
+        let userPostToDelete = newState.user.filter(post => {
         return post._id === action.postId
       })[0];
       let idx2 = newState.user.indexOf(userPostToDelete);
       if (idx2 !== -1) newState.user.splice(idx2, 1);
+      }
+      
       return newState;
     default:
       return state;
