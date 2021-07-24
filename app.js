2d01c974ea64e905bd2e361f0fab020a4cd86c94
JosephASandoval <josephasandoval17@gmail.com>
2021-07-24 14:36:44 -0700

    updated

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
-
 const db = require("./config/keys").mongoURI;
 const bodyParser = require("body-parser");
 const User = require("./models/User");
